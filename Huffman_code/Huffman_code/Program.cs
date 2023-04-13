8ddd892269f3bda03a449fb1e96a277d3ef22939
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-04-13 12:33:22 +0300

    Counting symbol occurrences in text

---
+++ Huffman_code/Huffman_code/Program.cs
@@ -0,0 +1,14 @@
+﻿Dictionary<char, int> symbolCount = new Dictionary<char, int>();
+var code = new Dictionary<string, string>();
+string text = File.ReadAllText("C:\\Users\\danya\\Huffman-code\\Huffman_code\\Huffman_code\\sherlock.txt");
+foreach (char symbol in text)
+{
+    if (!symbolCount.ContainsKey(symbol))
+    {
+        symbolCount.Add(symbol, 1);
+    }
+    else
+    {
+        symbolCount[symbol]++;
+    }
+}

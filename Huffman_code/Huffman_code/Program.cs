7bf2ae77a78b5b908d0017d9237c39adb03357e1
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-04-13 12:34:51 +0300

    Created minheap class

--- Huffman_code/Huffman_code/Program.cs
+++ Huffman_code/Huffman_code/Program.cs
@@ -27,3 +27,101 @@ class MinHeapNode
         RightNode = null;
     }
 }
+
+class MinHeap
+{
+    private MinHeapNode[] Objects;
+    public int Capacity;
+
+    public MinHeap(int capacity)
+    {
+        Objects = new MinHeapNode[capacity];
+    }
+
+    private int LeftChildIndex(int index)
+    {
+        return 2 * index + 1;
+    }
+    private MinHeapNode LeftChild(int index)
+    {
+        return Objects[2 * index + 1];
+    }
+    private int RightChildIndex(int index)
+    {
+        return 2 * index + 2;
+    }
+    private MinHeapNode RightChild(int index)
+    {
+        return Objects[2 * index + 2];
+    }
+    private int ParentIndex(int index)
+    {
+        return (index - 1) / 2;
+    }
+    private MinHeapNode Parent(int index)
+    {
+        return Objects[(index - 1) / 2];
+    }
+
+    private void Swap(int firstIndex, int secondIndex)
+    {
+        (Objects[firstIndex], Objects[secondIndex]) = (Objects[secondIndex], Objects[firstIndex]);
+    }
+
+    public void Add(MinHeapNode node)
+    {
+        if (Capacity == Objects.Length) return;
+
+        Objects[Capacity] = node;
+        Capacity++;
+
+        int index = Capacity - 1;
+        while (index != 0 && Objects[index].SymbolCount < Parent(index).SymbolCount)
+        {
+            int parentInd = ParentIndex(index);
+            Swap(parentInd, index);
+            index = parentInd;
+        }
+    }
+
+    public MinHeapNode Peek()
+    {
+        if (Capacity != 0)
+        {
+            return Objects[0];
+        }
+        else
+        {
+            throw new Exception("There are no objects in heap");
+        }
+    }
+
+    public MinHeapNode Pop()
+    {
+        if (Capacity == 0) return null;
+
+        var result = Objects[0];
+        Objects[0] = Objects[Capacity - 1];
+        Capacity--;
+
+        int index = 0;
+        while (LeftChildIndex(index) < Capacity)
+        {
+            int min = LeftChildIndex(index);
+            if (LeftChild(index).SymbolCount > RightChild(index).SymbolCount)
+            {
+                min = RightChildIndex(index);
+            }
+
+            if (Objects[min].SymbolCount >= Objects[index].SymbolCount)
+            {
+                break;
+            }
+
+            Swap(min, index);
+            index = min;
+        }
+
+        return result;
+    }
+}
